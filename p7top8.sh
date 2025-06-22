0e60f5c9fe1a269496054d1f24c7363d1ffa1488
Blue <felicisimone9@gmail.com>
2025-06-22 16:42:38 +0200

    Update p7top8.sh

--- p7top8.sh
+++ p7top8.sh
@@ -1,67 +1,60 @@
 #!/usr/bin/env bash
 
 convert_flag=false
-
 while getopts ":c" opt; do
   case $opt in
-    c)
-      convert_flag=true
-      ;;
-    \?)
-      echo "Usage: $0 [-c]"
-      exit
-      ;;
+    c) convert_flag=true ;;
+    \?) echo "Usage: $0 [-c]" >&2; exit 1 ;;
   esac
 done
 
 detector() {
   local to_convert=()
+  while IFS= read -r -d '' file; do
+    if mediainfo --Inform="Video;%HDR_Format_String%" "$file" | grep -q "Profile 7\.6"; then
+      to_convert+=("$file")
+      echo "[+] p7 detected: $file" >&2
+    fi
+  done < <(find . -type f -name '*.mkv' -print0)
 
-  readarray -t list <<< $(find . -type f -name '*.mkv')
-  IFS=','
-  for i in "${list[@]}"; do
-    mediaout=$(mediainfo $i | grep "HDR format")
-    read -ra split <<< "$mediaout"
-    for val in "${split[@]}"; do
-      val=$(echo "$val" | xargs)
-      if [ "$val" = "Profile 7.6" ]; then
-        to_convert+=("$i")
-        echo "[+] p7 detected: $i" >&2
-        break
-      fi
-    done
-  done
-for path in "${to_convert[@]}"; do
-  echo "$path"
-done
+  printf '%s\0' "${to_convert[@]}"
 }
 
 convert() {
   local file=$1
-  local dir=$(dirname "$file")
+  local dir
+  dir=$(dirname "$file")
+  local track_id
 
-  echo "[*] Extracting HDR P7 track from \"$file\" to \"$dir/film_p7.hevc\""
-  mkvextract tracks "$file" 0:"$dir/film_p7.hevc"
+  track_id=$(
+    mkvmerge -i "$file" |
+    awk -F':' '/video/ {
+      sub(/.*Track ID /, "", $1)
+      print $1
+      exit
+    }'
+  )
 
-  echo "[*] Converting \"$dir/film_p7.hevc\" → \"$dir/film_p8.hevc\""
+  echo "[*] Extracting HDR P7 track from \"$file\""
+  mkvextract tracks "$file" "${track_id}":"$dir/film_p7.hevc"
+
+  echo "[*] Converting p7 track to p8"
   dovi_tool -m 2 convert --discard "$dir/film_p7.hevc" -o "$dir/film_p8.hevc"
 
-  echo "[*] Merging new P8 track into \"$file.p8\""
+  echo "[*] Merging P8 track"
   mkvmerge -o "$file.p8" "$dir/film_p8.hevc" --no-video "$file"
 
-  echo "[-] Removing temporary files: film_p7.hevc & film_p8.hevc"
+  echo "[-] Cleaning"
   rm -f "$dir/film_p7.hevc" "$dir/film_p8.hevc"
-
-  echo "[*] Replacing original with converted file"
   mv "$file.p8" "$file"
 
   echo "[+] Conversion complete for \"$file\""
 }
 
-readarray -t movies <<< $(detector)
+mapfile -d '' movies < <(detector)
 
 if $convert_flag; then
   for mov in "${movies[@]}"; do
-    (convert $mov)
+    convert "$mov"
   done
 fi
